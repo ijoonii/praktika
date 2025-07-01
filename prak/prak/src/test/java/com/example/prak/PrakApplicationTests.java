fca6791a9a0ed7bede1acb8ecf07107537719e4c
mail@gitflic.ru <exxt_ye@mail.ru>
2025-07-01 17:03:02 +0400

    idi nahuy

---
+++ prak/prak/src/test/java/com/example/prak/PrakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.prak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
